beed3bd30725c84aacf45e44be82ca670cecf863
justcuong <47757215+justcuong@users.noreply.github.com>
2021-05-16 21:49:29 -0700

    Add files via upload

---
+++ Nguyen-Cuong-Project-2048/Nguyen-Cuong-Project-2048/Models/GameModel.swift
@@ -0,0 +1,339 @@
+import Foundation
+
+protocol GameModelProtocol : class {
+    func setTiles(to tiles: [((Int, Int), Int)])
+    func scoreChanged(to score: Int)
+}
+
+class GameModel {
+    
+    var gameboard: GameBoard
+    
+    unowned let delegate: GameModelProtocol
+    
+    var score = 0 {
+        didSet {
+            if UserDefaults.standard.integer(forKey: "HighScore") < score {
+                UserDefaults.standard.set(score, forKey: "HighScore")
+            }
+            delegate.scoreChanged(to: score)
+        }
+    }
+    
+    init(delegate: GameModelProtocol) {
+        gameboard = GameBoard(initialValue: .empty)
+        self.delegate = delegate
+    }
+    
+    func reset() {
+        score = 0
+        gameboard.setAll(to: .empty)
+    }
+    
+    func saveDataToArray() {
+        var data = [Int]()
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if case .empty = gameboard[i, j] {
+                    data.append(0)
+                }
+                if case let .tile(value) = gameboard[i, j] {
+                    data.append(value)
+                }
+            }
+        }
+        UserDefaults.standard.set(data, forKey: "TilesData")
+    }
+    
+    func saveScore() {
+        UserDefaults.standard.set(score, forKey: "Score")
+    }
+    
+    func restoreData(from data: [Int]) {
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if data[gameboard.dimension * i + j] == 0 {
+                    gameboard[i, j] = .empty
+                } else {
+                    gameboard[i, j] = .tile(data[gameboard.dimension * i + j])
+                }
+            }
+        }
+        
+        delegate.setTiles(to: getArrayOfCurrentTiles())
+    }
+    
+    func restoreScore(from score: Int) {
+        self.score = score
+        delegate.scoreChanged(to: self.score)
+    }
+
+    func insertTile(at index: (Int, Int), with value: Int) {
+        let (x, y) = index
+        gameboard[x, y] = .tile(value)
+        delegate.setTiles(to: getArrayOfCurrentTiles())
+        saveDataToArray()
+        saveScore()
+    }
+    
+    func insertTileAtRandomPosition(with value: Int) {
+        let emptyPositions = getEmptyPositions()
+        if !emptyPositions.isEmpty {
+            let randomIndex = Int(arc4random_uniform(UInt32(emptyPositions.count - 1)))
+            let (x, y) = emptyPositions[randomIndex]
+            insertTile(at: (x, y), with: value)
+        }
+    }
+    
+    func getEmptyPositions() -> [(Int, Int)] {
+        var buffer = [(Int, Int)]()
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if case .empty = gameboard[i, j] {
+                    buffer.append((i, j))
+                }
+            }
+        }
+        return buffer
+    }
+    
+    func userHasWon() -> Bool {
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if case .tile(2048) = gameboard[i, j] {
+                    return true
+                }
+            }
+        }
+        return false
+    }
+    
+    func userHasLost() -> Bool {
+        if !getEmptyPositions().isEmpty {
+            return false
+        }
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if case let .tile(v) = gameboard[i, j] {
+                    if tileBelowHasSameValue(at: (i, j), value: v) ||
+                        tileToRightHasSameValue(at: (i, j), value: v) {
+                        return false
+                    }
+                }
+            }
+        }
+        return true
+    }
+    
+    func tileBelowHasSameValue(at index: (Int, Int), value: Int) -> Bool {
+        let (x, y) = index
+        if y + 1 >= gameboard.dimension {
+            return false
+        }
+        if case let .tile(v) = gameboard[x, y + 1]   {
+            return v == value
+        }
+        return false
+    }
+    
+    func tileToRightHasSameValue(at index: (Int, Int), value: Int) -> Bool {
+        let (x, y) = index
+        if x + 1 >= gameboard.dimension {
+            return false
+        }
+        if case let .tile(v) = gameboard[x + 1, y]   {
+            return v == value
+        }
+        return false
+    }
+    
+    func performMove(direction: MoveDirection) -> Bool {
+        switch direction {
+        case .up:
+            let resultForSlide1 = slideUp()
+            let resultForCombine = combineUp()
+            let resultForSlide2 = slideUp()
+            return resultForSlide1 || resultForCombine || resultForSlide2
+        case .down:
+            let resultForSlide1 = slideDown()
+            let resultForCombine = combineDown()
+            let resultForSlide2 = slideDown()
+            return resultForSlide1 || resultForCombine || resultForSlide2
+        case .left:
+            let resultForSlide1 = slideLeft()
+            let resultForCombine = combineLeft()
+            let resultForSlide2 = slideLeft()
+            return resultForSlide1 || resultForCombine || resultForSlide2
+        case .right:
+            let resultForSlide1 = slideRight()
+            let resultForCombine = combineRight()
+            let resultForSlide2 = slideRight()
+            return resultForSlide1 || resultForCombine || resultForSlide2
+        }
+    }
+    
+    func slideUp() -> Bool {
+        var atLeastOneMove = false
+        for j in 0..<gameboard.dimension {
+             for _ in 0...gameboard.dimension - 1 {
+                for i in 0..<gameboard.dimension - 1 {
+                    if case .empty = gameboard[i, j] {
+                        if case .tile(_) = gameboard[i + 1, j] {
+                            atLeastOneMove = true
+                        }
+                        gameboard[i, j] = gameboard[i + 1, j]
+                        gameboard[i + 1, j] = .empty
+                    }
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func slideDown() -> Bool {
+        var atLeastOneMove = false
+        for j in 0..<gameboard.dimension {
+            for _ in 0...gameboard.dimension - 1{
+                for i in stride(from: gameboard.dimension - 1, to: 0, by: -1) {
+                    if case .empty = gameboard[i, j] {
+                        if case .tile(_) = gameboard[i - 1, j] {
+                            atLeastOneMove = true
+                        }
+                        gameboard[i, j] = gameboard[i - 1, j]
+                        gameboard[i - 1, j] = .empty
+                    }
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func slideLeft() -> Bool {
+        var atLeastOneMove = false
+        for i in 0..<gameboard.dimension {
+            for _ in 0...gameboard.dimension - 1 {
+                for j in 0..<gameboard.dimension - 1 {
+                    if case .empty = gameboard[i, j] {
+                        if case .tile(_) = gameboard[i, j + 1] {
+                            atLeastOneMove = true
+                        }
+                        gameboard[i, j] = gameboard[i, j + 1]
+                        gameboard[i, j + 1] = .empty
+                    }
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func slideRight() -> Bool {
+        var atLeastOneMove = false
+        for i in 0..<gameboard.dimension {
+            for _ in 0...gameboard.dimension - 1 {
+                for j in stride(from: gameboard.dimension - 1, to: 0, by: -1) {
+                    if case .empty = gameboard[i, j] {
+                        if case .tile(_) = gameboard[i, j - 1] {
+                            atLeastOneMove = true
+                        }
+                        gameboard[i, j] = gameboard[i, j - 1]
+                        gameboard[i, j - 1] = .empty
+                    }
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func combineUp() -> Bool {
+        var atLeastOneMove = false
+        for j in 0..<gameboard.dimension {
+            for i in 0..<gameboard.dimension - 1 {
+                if gameboard[i + 1, j].getValue() == gameboard[i, j].getValue() && gameboard[i + 1, j].getValue() != nil {
+                    score += gameboard[i + 1, j].getValue()!
+                    let newValue: Int = 2 * gameboard[i + 1, j].getValue()!
+                    gameboard[i, j] = .tile(newValue)
+                    gameboard[i + 1, j] = .empty
+                    atLeastOneMove = true
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func combineDown() -> Bool {
+        var atLeastOneMove = false
+        for j in 0..<gameboard.dimension {
+            for i in stride(from: gameboard.dimension - 1, to: 0, by: -1) {
+                if gameboard[i - 1, j].getValue() == gameboard[i, j].getValue() && gameboard[i - 1, j].getValue() != nil {
+                    score += gameboard[i - 1, j].getValue()!
+                    let newValue: Int = 2 * gameboard[i - 1, j].getValue()!
+                    gameboard[i, j] = .tile(newValue)
+                    gameboard[i - 1, j] = .empty
+                    atLeastOneMove = true
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func combineLeft() -> Bool {
+        var atLeastOneMove = false
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension - 1 {
+                if gameboard[i, j].getValue() == gameboard[i, j + 1].getValue() && gameboard[i, j].getValue() != nil {
+                    score += gameboard[i, j].getValue()!
+                    let newValue: Int = 2 * gameboard[i, j].getValue()!
+                    gameboard[i, j] = .tile(newValue)
+                    gameboard[i, j + 1] = .empty
+                    atLeastOneMove = true
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func combineRight() -> Bool {
+        var atLeastOneMove = false
+        for i in 0..<gameboard.dimension {
+            for j in stride(from: gameboard.dimension - 1, to: 0, by: -1) {
+                if gameboard[i, j].getValue() == gameboard[i, j - 1].getValue() && gameboard[i, j].getValue() != nil {
+                    score += gameboard[i, j].getValue()!
+                    let newValue: Int = 2 * gameboard[i, j].getValue()!
+                    gameboard[i, j] = .tile(newValue)
+                    gameboard[i, j - 1] = .empty
+                    atLeastOneMove = true
+                }
+            }
+        }
+        return atLeastOneMove
+    }
+    
+    func printCurrentGameBoardToConsole() {
+        for i in 0..<gameboard.dimension {
+            var line = ""
+            for j in 0..<gameboard.dimension {
+                if case .empty = gameboard[i, j] {
+                    line += "0"
+                }
+                if case let .tile(v) = gameboard[i, j] {
+                    line += String(v)
+                }
+                line += " "
+            }
+            print(line)
+        }
+    }
+    
+    func getArrayOfCurrentTiles() -> [((Int, Int), Int)] {
+        var buffer = [((Int, Int), Int)]()
+        for i in 0..<gameboard.dimension {
+            for j in 0..<gameboard.dimension {
+                if case let .tile(v) = gameboard[i, j] {
+                    buffer.append(((i, j), v))
+                }
+            }
+        }
+        return buffer
+    }
+    
+}
---
+++ Nguyen-Cuong-Project-2048/Nguyen-Cuong-Project-2048/ViewController.swift
@@ -0,0 +1,187 @@
+import UIKit
+
+class ViewController: UIViewController, GameModelProtocol {
+    
+    var model: GameModel?
+    var board: GameBoardView?
+    
+    let dimension = 4
+    var boardWidth: CGFloat?
+    let padding: CGFloat = 7.0
+    let radius: CGFloat = 10.0
+    var width: CGFloat?
+    
+    var scoreView: ScoreView?
+    var highScoreView: ScoreView?
+    var restartGameButton = RestartGameButton()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        model = GameModel(delegate: self)
+        setupGame()
+        restoreDate()
+        setupButton()
+        setupScoreView()
+        setupHighScoreView()
+    }
+    
+    
+    @IBAction func swipeGestureLeft(_ sender: UISwipeGestureRecognizer) {
+        if model!.performMove(direction: MoveDirection.left) {
+            followUp()
+        }
+    }
+    
+    @IBAction func swipeGestureRight(_ sender: UISwipeGestureRecognizer) {
+        if model!.performMove(direction: MoveDirection.right) {
+            followUp()
+        }
+    }
+    
+    @IBAction func swipeGestureUp(_ sender: UISwipeGestureRecognizer) {
+        if model!.performMove(direction: MoveDirection.up) {
+            followUp()
+        }
+    }
+    
+    @IBAction func swipeGestureDown(_ sender: UISwipeGestureRecognizer) {
+        if model!.performMove(direction: MoveDirection.down) {
+            followUp()
+        }
+    }
+    
+    
+    func setupGame() {
+        let vcHeight = view.bounds.size.height
+        let vcWidth = view.bounds.size.width
+        boardWidth = vcWidth - padding * 2
+        width = CGFloat(floorf(CFloat(boardWidth! - padding * CGFloat(dimension + 1))))/CGFloat(dimension)
+        board = GameBoardView(tileWidth: width!, tilePadding: padding, cornerRadius: radius)
+        view.backgroundColor = UIColor(red: 242/255, green: 243/255, blue: 245/255, alpha: 1.0)
+        
+        func xPositionToCenterView(_ v: UIView) -> CGFloat {
+            let viewWidth = v.bounds.size.width
+            let tentativeX = (vcWidth - viewWidth)/2
+            return tentativeX >= 0 ? tentativeX : 0
+        }
+        
+        func yPositionToCenterView(_ v: UIView) -> CGFloat {
+            let viewHeight = v.bounds.size.height
+            let tentativeY = (vcHeight - viewHeight)/2
+            return tentativeY >= 0 ? tentativeY : 0
+        }
+        
+        var f = board!.frame
+        f.origin.x = xPositionToCenterView(board!)
+        f.origin.y = yPositionToCenterView(board!)
+        board!.frame = f
+        
+        view.addSubview(board!)
+    
+    }
+    
+    func followUp() {
+        let randomVal = Int(arc4random_uniform(10))
+        model!.insertTileAtRandomPosition(with: randomVal == 1 ? 4 : 2)
+        
+        if model!.userHasLost() {
+            let alertControllerForLose = UIAlertController(title: "Defeat", message: "You lost 💀", preferredStyle: .alert)
+            
+            let actionRestart = UIAlertAction(title: "Try again", style: .default) {
+                UIAlertAction in
+                self.restartGame()
+            }
+            
+            alertControllerForLose.addAction(actionRestart)
+            
+            self.present(alertControllerForLose, animated: true, completion: nil)
+        }
+        
+        if model!.userHasWon() && !UserDefaults.standard.bool(forKey: "HasWonOnce") {
+            UserDefaults.standard.set(true, forKey: "HasWonOnce")
+            let alertControllerForWin = UIAlertController(title: "Victory!", message: "You won", preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: "Continue playing", style: .default) {
+                UIAlertAction in
+            }
+            
+            alertControllerForWin.addAction(action)
+            
+            self.present(alertControllerForWin, animated: true, completion: nil)
+        }
+    }
+    
+    func restartGame() {
+        UserDefaults.standard.set(false, forKey: "HasWonOnce")
+        self.model!.reset()
+        self.board!.resetTheView()
+        self.model!.insertTileAtRandomPosition(with: 2)
+        self.model!.insertTileAtRandomPosition(with: 2)
+    }
+    
+    func restoreDate() {
+        if UserDefaults.standard.array(forKey: "TilesData") == nil || UserDefaults.standard.array(forKey: "TilesData")!.isEmpty {
+            model!.insertTileAtRandomPosition(with: 2)
+            model!.insertTileAtRandomPosition(with: 2)
+        } else {
+            model!.restoreData(from: UserDefaults.standard.array(forKey: "TilesData")  as! [Int])
+            model!.restoreScore(from: UserDefaults.standard.integer(forKey: "Score"))
+        }
+    }
+    
+    func setTiles(to tiles: [((Int, Int), Int)]) {
+        board!.setTiles(to: tiles)
+    }
+    
+    func scoreChanged(to score: Int) {
+        if scoreView != nil {
+            scoreView!.score = score
+        }
+        if highScoreView != nil {
+            highScoreView!.score = UserDefaults.standard.integer(forKey: "HighScore")
+        }
+    }
+    
+    func setupButton() {
+        view.addSubview(restartGameButton)
+        restartGameButton.translatesAutoresizingMaskIntoConstraints = false
+        restartGameButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        restartGameButton.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        restartGameButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        restartGameButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50).isActive = true
+        addActionToRestartButton()
+    }
+    
+    func setupScoreView() {
+        scoreView = ScoreView(width: 280, height: 50, radius: 9.0, substring: "Score")
+        view.addSubview(scoreView!)
+
+        scoreView!.translatesAutoresizingMaskIntoConstraints = false
+        scoreView!.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        scoreView!.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        scoreView!.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        scoreView!.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+    }
+    
+    func setupHighScoreView() {
+        highScoreView = ScoreView(width: 280, height: 50, radius: 9.0, substring: "High score")
+        highScoreView!.score = UserDefaults.standard.integer(forKey: "HighScore")
+        view.addSubview(highScoreView!)
+        
+        highScoreView!.translatesAutoresizingMaskIntoConstraints = false
+        highScoreView!.widthAnchor.constraint(equalToConstant: 280).isActive = true
+        highScoreView!.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        highScoreView!.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        highScoreView!.topAnchor.constraint(equalTo: view.topAnchor, constant: 105).isActive = true
+    }
+    
+    func addActionToRestartButton() {
+        restartGameButton.addTarget(self, action: #selector(restartGameButtonTapped), for: .touchUpInside)
+    }
+    
+    @objc func restartGameButtonTapped() {
+        restartGameButton.shake()
+        restartGame()
+    }
+}
+
